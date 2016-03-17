e5450663124ef5d800aac7944e8435300dab38b9
impro <impro.box@gmail.com>
2016-03-17 10:57:29 +0200

    Travis

--- models/model-submit-form.php
+++ models/model-submit-form.php
@@ -1,17 +1,17 @@
 	<?php
 	/**
-	* Model for submite form.
-	*
-	* @package    Cherry_Framework
-	* @subpackage Model
-	* @author     Cherry Team <cherryframework@gmail.com>
-	* @copyright  Copyright (c) 2012 - 2016, Cherry Team
-	* @link       http://www.cherryframework.com/
-	* @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
-	*/
+	 * Model for submite form.
+	 *
+	 * @package    Cherry_Framework
+	 * @subpackage Model
+	 * @author     Cherry Team <cherryframework@gmail.com>
+	 * @copyright  Copyright (c) 2012 - 2016, Cherry Team
+	 * @link       http://www.cherryframework.com/
+	 * @license    http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+	 */
 
 	/**
-	* Model properties
+	 * Model properties
 	*/
 	class Model_Submit_Form {
 
@@ -32,7 +32,7 @@
 
 		wp_localize_script( 'cherry-js-core', 'form_url', 
 			array(
-				'url' => admin_url('admin-ajax.php'),
+				'url' => admin_url( 'admin-ajax.php' ),
 			)
 		);
 		$terms = Model_Submit_Form::custom_taxonomy_walker( 'property-type' );
@@ -56,8 +56,7 @@
 		$file = $_FILES['thumb'];
 		$attachment_id = Model_Submit_Form::insert_attacment( $file, $post_id );
 
-		 
-		$files = Model_Submit_Form::reArrayFiles( $_FILES['gallery'] );
+		$files = Model_Submit_Form::re_array_files( $_FILES['gallery'] );
 		foreach ( $files as $key => $file ) {
 			$property_meta['gallery']['image'][ $key ] = Model_Submit_Form::insert_attacment( $file, $post_id );
 		}
@@ -75,7 +74,7 @@
 
 		require_once( ABSPATH . 'wp-admin/includes/admin.php' );
 		$file_return = wp_handle_upload( $file, array( 'test_form' => false ) );
-		if( isset( $file_return['error'] ) || isset( $file_return['upload_error_handler'] ) ) {
+		if ( isset( $file_return['error'] ) || isset( $file_return['upload_error_handler'] ) ) {
 			return false;
 		} else {
 			$filename = $file_return['file'];
@@ -84,13 +83,13 @@
 				'post_title' => preg_replace( '/\.[^.]+$/', '', basename( $filename ) ),
 				'post_content' => '',
 				'post_status' => 'inherit',
-				'guid' => $file_return['url']
+				'guid' => $file_return['url'],
 			);
 			$attachment_id = wp_insert_attachment( $attachment, $file_return['url'], $post_id );
 			require_once( ABSPATH . 'wp-admin/includes/image.php' );
 			$attachment_data = wp_generate_attachment_metadata( $attachment_id, $filename );
 			wp_update_attachment_metadata( $attachment_id, $attachment_data );
-			if( 0 < intval( $attachment_id ) ) {
+			if ( 0 < intval( $attachment_id ) ) {
 				return $attachment_id;
 			}
 		}
@@ -101,7 +100,7 @@
 		$terms = get_terms( $taxonomy, array( 'parent' => $parent, 'hide_empty' => false ) );
 		if ( count( $terms ) > 0) {
 			foreach ( $terms as $term ) {
-				$child = Model_Submit_Form::custom_taxonomy_walker( $taxonomy, $term->term_id ); 
+				$child = Model_Submit_Form::custom_taxonomy_walker( $taxonomy, $term->term_id );
 				$types[] = array(
 					'term_id' => $term->term_id,
 					'name' => $term->name,
@@ -121,7 +120,6 @@
 			'1.0.0',
 			false
 		);
-		
 		wp_enqueue_script(
 			'load_image',
 			plugins_url( 'tm-real-estate' ) . '/assets/js/uploader/load-image.all.min.js',
@@ -172,7 +170,7 @@
 		//	'all'
 		//);
 	}
-	public function reArrayFiles( &$file_post ) {
+	public function re_array_files( &$file_post ) {
 		$file_array = array();
 		$file_count = count( $file_post['name'] );
 		$file_keys = array_keys( $file_post );
