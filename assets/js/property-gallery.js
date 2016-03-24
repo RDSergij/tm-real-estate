f9b2d60a1fe307470914e10377d60bc0b6240a21
RDSergij <sergijlys@gmail.com>
2016-03-24 15:12:32 +0200

    Small fix

--- assets/js/property-gallery.js
+++ assets/js/property-gallery.js
@@ -1,11 +1,11 @@
 jQuery( document ).ready( function() {
-	var galleryTop = new winodw.Swiper( '.properties .property-item .gallery-top', {
+	var galleryTop = new window.Swiper( '.properties .property-item .gallery-top', {
 		nextButton: '.swiper-button-next',
 		prevButton: '.swiper-button-prev',
 		spaceBetween: 10
 	});
 
-	var galleryThumbs = new winodw.Swiper( '.properties .property-item .gallery-thumbs', {
+	var galleryThumbs = new window.Swiper( '.properties .property-item .gallery-thumbs', {
 		spaceBetween: 10,
 		centeredSlides: true,
 		slidesPerView: 'auto',
