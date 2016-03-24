0b0a14a7d7802fc8ed338b9d920176b081afc160
RDSergij <sergijlys@gmail.com>
2016-03-24 15:05:03 +0200

    Travis-huievis

--- assets/js/property-gallery.js
+++ assets/js/property-gallery.js
@@ -1,17 +1,19 @@
-jQuery( document ).ready(function(){
-	var galleryTop = new Swiper('.gallery-top', {
+jQuery( document ).ready( function() {
+	var galleryTop = new Swiper( '.properties .property-item .gallery-top', {
 		nextButton: '.swiper-button-next',
 		prevButton: '.swiper-button-prev',
-		spaceBetween: 10,
+		spaceBetween: 10
 	});
-	var galleryThumbs = new Swiper('.gallery-thumbs', {
+
+	var galleryThumbs = new Swiper( '.properties .property-item .gallery-thumbs', {
 		spaceBetween: 10,
 		centeredSlides: true,
 		slidesPerView: 'auto',
 		touchRatio: 0.2,
 		slideToClickedSlide: true
 	});
-	galleryTop.params.control = galleryThumbs;
-	galleryThumbs.params.control = galleryTop;
-	
+
+	galleryTop.params.control		= galleryThumbs;
+	galleryThumbs.params.control	= galleryTop;
+
 });
