f37f5f95976ceec8e9b2604ba72583ad1d0e7421
impro <impro.box@gmail.com>
2016-03-29 14:50:51 +0300

    travis

--- assets/js/submit-form.js
+++ assets/js/submit-form.js
@@ -3,80 +3,74 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-jQuery(document).ready( function( ) {
+jQuery( document ).ready( function( ) {
 
-	var progressBar = jQuery( "#progress" )
-	var filesData = []
+	var progressBar = jQuery( "#progress" );
+	var filesData = [];
 	var filesCount = 0;
-	jQuery( function ( $ ) {
+	jQuery( function( $ ) {
 		'use strict';
 
-		// Change this to the location of your server-side upload handler:
-		var url = window.location.hostname === 'blueimp.github.io';
-		$('#galery').fileupload({
+
+		$( '#galery' ).fileupload({
 			filesCount: 0,
-			url: url,
 			dataType: 'json',
 			autoUpload: false,
 			acceptFileTypes: /(\.|\/)(gif|jpe?g|png)$/i,
 			maxFileSize: 999000,
-			// Enable image resizing, except for Android and Opera,
-			// which actually support image resizing, but fail to
-			// send Blob objects via XHR requests:
-			disableImageResize: /Android(?!.*Chrome)|Opera/
-					.test(window.navigator.userAgent),
+			disableImageResize: /Android(?!.*Chrome)|Opera/.test( window.navigator.userAgent ),
 			previewMaxWidth: 100,
 			previewMaxHeight: 100,
 			previewCrop: true
-		}).on('fileuploadadd', function (e, data) {
-			data.context = $('<div class="col-xs-6 col-sm-2">').appendTo('#files');
+		}).on( 'fileuploadadd', function ( e, data ) {
+			data.context = $( '<div class="col-xs-6 col-sm-2">' ).appendTo( '#files' );
 
-			$.each(data.files, function (index, file) {
-				var node = $('<p class="text-center"/>')
-						.append($('<span/>').text(file.name))
-						.append($('<input type="text" name="property[meta][gallery][title][' + filesCount + ']"/><span class="close" data-index="' + filesCount + '" ></span>'));
-				filesData[filesCount] = file;
+			$.each( data.files, function( index, file ) {
+				var node = $( '<p class="text-center"/>' )
+						.append( $( '<span/>' ).text( file.name ) )
+						.append( $( '<input type="text" name="property[meta][gallery][title][' + filesCount + ']"/><span class="close" data-index="' + filesCount + '" ></span>' ) );
+				filesData[ filesCount ] = file;
 				filesCount++;
 
-				if (!index) {
-					node.append('<br>')
+				if ( ! index ) {
+					node.append( '<br>' )
 				}
-				node.appendTo(data.context);
+				node.appendTo( data.context );
 			});
-		}).on('fileuploadprocessalways', function (e, data) {
+		}).on( 'fileuploadprocessalways', function( e, data ) {
 			var index = data.index,
-					file = data.files[index],
-					node = $(data.context.children()[index]);
-			if (file.preview) {
+					file = data.files[ index ],
+					node = $( data.context.children()[ index ] );
+			if ( file.preview ) {
 				node
-						.prepend('<br>')
-						.prepend(file.preview);
+						.prepend( '<br>' )
+						.prepend( file.preview );
 			}
-			if (file.error) {
+			if ( file.error ) {
 				node
-						.append('<br>')
-						.append($('<span class="text-danger"/>').text(file.error));
+						.append( '<br>' )
+						.append( $( '<span class="text-danger"/>' ).text( file.error ) );
 			}
-			if (index + 1 === data.files.length) {
-				data.context.find('button')
-						.text('Upload')
-						.prop('disabled', !!data.files.error);
+			if ( index + 1 === data.files.length ) {
+				data.context.find( 'button' )
+						.text( 'Upload' )
+						.prop( 'disabled', !!data.files.error );
 			}
 		});
 
-	}(jQuery));
-	jQuery(document).on('click', 'span.close', function () {
-		filesData.splice($(this).data('index'), 1);
+	}( jQuery ));
+	jQuery( document ).on( 'click', 'span.close', function( ) {
+		filesData.splice( $(this).data('index'), 1 );
 		jQuery(this).parent().parent().remove();
 	})
-	jQuery('#property_submit_format').on('submit', function (event) {
-		formData = new FormData(this);
+	jQuery( '#property_submit_format' ).on( 'submit', function ( event ) {
+		formData = new FormData( this );
 		if ( filesCount ) {
 			for( var i = 0; i < filesCount; i ++ ) {
-				formData.append('gallery[' + i + ']', filesData[ i ] );
+				formData.append( 'gallery[' + i + ']', filesData[ i ] );
 			}
 		}
-		event.preventDefault();
+		event.preventDefault( );
 		jQuery.ajax({
 			url: form_url.url + '?action=submit_form',
 			processData: false,
@@ -86,15 +80,15 @@ jQuery(document).ready( function( ) {
 			data: formData,
 			success: function(data){
 				responce =  JSON.parse(data);
-				jQuery('.tm-form-preloader').css('display','none');
+				jQuery( '.tm-form-preloader' ).css( 'display','none' );
 				if(responce['success']){
-					jQuery('#property_submit_format').replaceWith("<div>" + responce['data'] + "</div>" );
+					jQuery( '#property_submit_format' ).replaceWith("<div>" + responce['data'] + "</div>" );
 				}else{
-					jQuery('#property_submit_format').replaceWith("<div>" + responce['data'] + "</div>" );
+					jQuery( '#property_submit_format' ).replaceWith("<div>" + responce['data'] + "</div>" );
 				}
 			},
 			beforeSend: function() {
-				jQuery('.tm-form-preloader').css('display','block');
+				jQuery( '.tm-form-preloader' ).css( 'display', 'block' );
 			}
 		})
 })
