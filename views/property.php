47f0aad3241a59d94d4e2e3604c8e3b6ab3b7c5a
RDSergij <sergijlys@gmail.com>
2016-04-14 10:19:37 +0300

    Add park places

--- views/property.php
+++ views/property.php
@@ -93,6 +93,9 @@
 						</span>
 						<?php echo $property->bedrooms; ?>
 					</li>
+					<li class="parking_places">
+						<?php echo $property->parking_places; ?>
+					</li>
 					<li class="area">
 						<span>
 							<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 24 24" enable-background="new 0 0 24 24" xml:space="preserve">
