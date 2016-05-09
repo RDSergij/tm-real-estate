4d25228c7942c2d237e9dd154f15b4142fb4da16
vytya <tugo@mail.ru>
2016-05-09 23:05:37 +0300

    UPD: properties scripts

--- assets/js/properties.js
+++ assets/js/properties.js
@@ -9,12 +9,17 @@ jQuery( document ).ready( function( $ ) {
 		};
 
 		pap.papFunction = function() {
-			jQuery( '.post-type-property .selectit' ).each(function(){
-				jQuery(this).find('input').after('<span></span>');
+			jQuery( '.post-type-property .selectit, .check-column' ).each(function(){
+				var span = jQuery(this).find('span');
+				if ( !span[0] ) {
+					jQuery(this).find('input[type="checkbox"]').after('<span></span>');
+				}
 			});
 		};
 	};
 
 	var pap = new pageAddProperty();
 	pap.init();
+
+	jQuery( document ).ajaxComplete(pap.init);
 } );
