1b5aaa9fff1b3e9a072f0f553645cd04e2bda607
RDSergij <sergijlys@gmail.com>
2016-05-12 10:40:08 +0300

    Fix settings js path

--- cherry-framework/modules/cherry-page-builder/cherry-page-builder.php
+++ cherry-framework/modules/cherry-page-builder/cherry-page-builder.php
@@ -367,7 +367,7 @@ if ( ! class_exists( 'Cherry_Page_Builder' ) ) {
 
 			wp_enqueue_script(
 				'cherry-settings-page',
-				$this->core->settings['base_dir'] . 'modules/' . $this->module_slug . '/assets/js/page-settings.min.js',
+				$this->core->settings['base_url'] . 'modules/' . $this->module_slug . '/assets/js/page-settings.min.js',
 				array( 'jquery' ),
 				'0.2.1',
 				true
@@ -375,7 +375,7 @@ if ( ! class_exists( 'Cherry_Page_Builder' ) ) {
 
 			wp_enqueue_style(
 				'cherry-settings-page',
-				$this->core->settings['base_dir'] . 'modules/' . $this->module_slug . '/assets/css/page-settings.min.css',
+				$this->core->settings['base_url'] . 'modules/' . $this->module_slug . '/assets/css/page-settings.min.css',
 				array(),
 				'0.1.0',
 				'all'
