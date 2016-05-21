be9fd3d651955c7874aa875a771f0448b3bce86a
Sergij Osadchy <sergijlys@gmail.com>
2016-05-21 16:40:35 +0300

    Add attribute to button shortcode_contact_form

--- models/model-shortcode-tinymce.php
+++ models/model-shortcode-tinymce.php
@@ -157,6 +157,21 @@ class Shortcode_Tinymce {
 				),
 			),
 		);
+		$view_settings[ Model_Main::SHORT_CODE_CONTACT_FORM ] = array(
+			'title'  => __( 'Contact form', 'tm-real-estate' ),
+			'image'  => '',
+			'width'  => 600,
+			'height' => 80,
+			'body'	 => array(
+				array(
+					'type'        => 'listbox',
+					'name'        => 'agent',
+					'value'       => '',
+					'label'  => __( 'Agent', 'tm-real-estate' ),
+					'values'     => Shortcode_Tinymce::tm_prepare_options( Model_Main::get_agents() ),
+				),
+			),
+		);
 		return $view_settings;
 	}
 
